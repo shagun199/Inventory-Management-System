4f3f8030dcd835f8b2c08417d7c6fe0745891db5
yashbhanu <bhanushaliyash2000@gmail.com>
2022-11-03 23:33:23 +0530

    Db connection

---
+++ _dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+// Script to connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "inventory";
+$conn = mysqli_connect($servername, $username, $password, $database);
+?>
