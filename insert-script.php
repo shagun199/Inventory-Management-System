bfac9fd9f6bf1913cd5b174f7fadfe3e0a7aa8e2
yashbhanu <bhanushaliyash2000@gmail.com>
2022-11-04 20:11:39 +0530

    enchantment

--- insert-script.php
+++ insert-script.php
@@ -4,13 +4,13 @@ if(isset($_POST['submit'])){
   $productName = $_POST['products'];
   $quantity = $_POST['quantity'];
 
-  $check_product = "SELECT quantity FROM `products` WHERE `productName` = '$productName'";
-  $conn->query($check_product);
-     if(!empty($productName) && !empty($quantity) && !empty(`quantity`)){
+  // $check_product = "SELECT quantity FROM `products` WHERE `productName` = '$productName'";
+  // $conn->query($check_product);
+     if( !empty("SELECT quantity FROM `products` WHERE `productName` = '$productName")){
       $sql = "UPDATE `products` SET `quantity` = `quantity` + '$quantity' WHERE `productName` = '$productName'";
       $conn->query($sql);
      }
-     else if(!empty($productName) && !empty($quantity)){
+     else if(!empty($productName) && !empty($quantity) && empty("SELECT quantity FROM `products` WHERE `productName` = '$productName")){
       $query = "INSERT INTO products (productName, quantity) VALUES('$productName', '$quantity')";
       $result = $conn->query($query);
      
