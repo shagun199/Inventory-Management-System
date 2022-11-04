ba398e9fc95fe346499de722d9836d21fb7ab463
yashbhanu <bhanushaliyash2000@gmail.com>
2022-11-04 20:00:43 +0530

    enchantment

--- insert-script.php
+++ insert-script.php
@@ -4,20 +4,25 @@ if(isset($_POST['submit'])){
   $productName = $_POST['products'];
   $quantity = $_POST['quantity'];
 
-  $check_product = "SELECT * FROM `products` WHERE `productName` = '$productName'";
-     if(!empty($productName) && !empty($quantity) && $quantity > 0 && $check_product != $productName){
-
+  $check_product = "SELECT quantity FROM `products` WHERE `productName` = '$productName'";
+  $conn->query($check_product);
+     if(!empty($productName) && !empty($quantity) && !empty($check_product)){
+      $sql = "UPDATE `products` SET `quantity` = `quantity` + '$quantity' WHERE `productName` = '$productName'";
+      $conn->query($sql);
+     }
+     else if(!empty($productName) && !empty($quantity)){
       $query = "INSERT INTO products (productName, quantity) VALUES('$productName', '$quantity')";
       $result = $conn->query($query);
      
       if($result){
         echo "Student detail is inserted successfully";
       }  
+     }
       else{
         echo "Student detail is not inserted successfully";
-      }    }
+      }    
   }
 ?>
 
 
-$sql = "UPDATE `products` SET `quantity` = `quantity` + '$quantity' WHERE `productName` = '$productName'";
+<!-- $sql = "UPDATE `products` SET `quantity` = `quantity` + '$quantity' WHERE `productName` = '$productName'"; -->
