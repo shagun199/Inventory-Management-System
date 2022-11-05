b2a88ea39cb68c5cf3191dc979b636ad41b67871
shagun199 <shagun199@gmail.com>
2022-11-05 19:55:37 +0530

    Solved issue related to page refresh

--- index.php
+++ index.php
@@ -76,7 +76,11 @@
   </div>
 
   </div>
-
+  <script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
 </body>
